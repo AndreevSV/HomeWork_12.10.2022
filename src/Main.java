87360db1278e965caf92ea9988f1496cb52b9295
AndreevSV <51971450+AndreevSV@users.noreply.github.com>
2022-10-16 16:03:12 +0300

    Corrected program after supervision by Michael. Thank you for your tip with initialising the array

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ import java.util.Arrays;
 
 
 public class Main {
+    static int[] arr = generateRandomArray();
+
     public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(arr)); // вызывается метод generateRandomArray для создания массива arr
+
         task1();
         task2();
         task3();
@@ -11,7 +16,7 @@ public class Main {
 
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
-        int[] arr = new int[3];
+        int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
         }
@@ -19,28 +24,21 @@ public class Main {
     }
 
     public static void task1() {
-        int[] arr = generateRandomArray(); // вызывается метод generateRandomArray для создания массива arr
-        System.out.println(Arrays.toString(arr)); // выводится на печать массив arr
         int sum = 0; // начальная сумма значений массива
         for (int i : arr) {
             sum += i; // Проходится массив и складывается каждое значение для последующего вывода суммы
         }
-        System.out.printf("Сумма затрат за %d дня(-ей) составит %d \n", arr.length, sum); // Вывод на печать суммы всех значений массива
+        System.out.printf("Сумма затрат за %d дней составит %d \n", arr.length, sum); // Вывод на печать суммы всех значений массива
     }
 
     public static void task2() {
-        int[] arr = generateRandomArray(); // вызывается метод generateRandomArray для создания массива arr
-        System.out.println(Arrays.toString(arr)); // выводится на печать массив arr
-        int min = 200_000; // инициализация переменной минимального значения для начала сравнения
-        int max = 0; // инициализация переменной максимального значения для начала сравнения
+        int min = arr[0]; // инициализация переменной минимального значения для начала сравнения
+        int max = arr[0]; // инициализация переменной максимального значения для начала сравнения
         for (int i : arr) { // Проходится весь массив
             if (i < min) {
-                min = i;
-            }
-        }
-        for (int i : arr) {
-            if (i > max) {
-                max = i;
+                min = i; // Находится минимальное значение
+            } if (i > max) {
+                max = i; // Находится максимальное значение
             }
         }
         System.out.printf("Минимальная сумма трат за день составила %d \n", min); // Вывод на печать минимального значения в массиве
@@ -48,15 +46,13 @@ public class Main {
     }
 
     public static void task3() {
-        int[] arr = generateRandomArray();
-        System.out.println(Arrays.toString(arr));
         int sum = 0;
         float average;
         for (int i : arr) {
             sum += i;
         }
             average = sum / arr.length;
-            System.out.println(average);
+            System.out.printf("Среднедневная сумма трат составила %.0f \n", average);
         }
 
     public static void task4() {
